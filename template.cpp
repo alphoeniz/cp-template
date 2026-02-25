f22b238ddf065861bea9ba63e23b8535ac871a46
alphoeniz <varungupta.codes@gmail.com>
2026-02-26 01:08:53 +0530

    updated template

--- template.cpp
+++ template.cpp
@@ -2,58 +2,57 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
 
-typedef unsigned long long ull;
-typedef long double ld;
-typedef pair<int,int> pii;
-typedef pair<double,double> pdd;
-typedef vector<int> vi;
-typedef set<int> si;
-typedef map<int,int> mii;
-typedef vector<pii> vii;
-typedef vector<vi> vvi;
-typedef vector<si> vsi;
-typedef set<pii> sii;
-typedef priority_queue<int> qi;
-typedef priority_queue<pii> qii;
-typedef priority_queue<pii, vii, greater<pii>> qgii;
+// --- TYPES ---
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using si = set<int>;
+using mii = map<int, int>;
+using vii = vector<pii>;
+using vvi = vector<vi>;
+using vsi = vector<si>;
+using sii = set<pii>;
+using qi = priority_queue<int>;
+using qii = priority_queue<pii>;
+using qgii = priority_queue<pii, vii, greater<pii>>;
 
-#define cin(x) int x; cin >> x;
-#define cin2(x,y) int x, y; cin >> x >> y;
+// --- MACROS ---
 #define in(n,a) vi a(n); for(int &x:a) cin >> x;
-#define out(a) for(int &x:a) cout << x << " "; cout << nl;
+#define out(a) for(int &x:a) cout << x << " "; cout << "\n";
 #define fo(i,a,b) for(int i = a; i < b; ++i)
 #define rfo(i,a,b) for(int i = a; i >= b; --i)
 
-#define lsb(n) (n & -n)
-#define msb(n) (1LL << (63 - __builtin_clzll(n)))
+#define lsb(n) ((n) & -(n))
+#define msb(n) (63 - __builtin_clzll(n)) 
 #define nsb(n) (__builtin_popcountll(n))
-#define len(n) (to_string(n).size())
-#define is2pow(n) (n && (!(n & (n-1))))
-
-#define nl "\n"
 #define sz(x) ((int)(x).size())
 #define all(a) a.begin(),a.end()
 #define rall(a) a.rbegin(),a.rend()
-#define maxe(a) (*max_element(all(a)))
-#define mine(a) (*min_element(all(a)))
 
-#define M map
-#define V vector
-#define P pair
 #define ff first
 #define ss second
 #define pb push_back
 #define ppb pop_back
+#define eb emplace_back
 #define mp make_pair
-#define lb lower_bound
-#define ub upper_bound
+#define lb lower_bound // >= 
+#define ub upper_bound // >
 
-#define pY  {cout << "YES\n"; return;}
-#define pN  {cout << "NO\n"; return;}
+#define nl "\n"
+#define maxe(a) (*max_element(all(a)))
+#define mine(a) (*min_element(all(a)))
+
+#define pY {cout << "YES\n"; return;}
+#define pN {cout << "NO\n"; return;}
 void show (bool x) {cout << ((x) ? "YES":"NO") << nl; return;}
 
+// --- DEBUG ---
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
 #else
@@ -61,11 +60,13 @@ void show (bool x) {cout << ((x) ? "YES":"NO") << nl; return;}
 #endif
 
 void _print(int t) {cerr << t;}
+void _print(ll t) {cerr << t;}
 void _print(string t) {cerr << t;}
 void _print(char t) {cerr << t;}
 void _print(ld t) {cerr << t;}
 void _print(double t) {cerr << t;}
 void _print(ull t) {cerr << t;}
+void _print(bool t){cerr << t;}
 
 template <class T, class V> void _print(pair <T, V> p);
 template <class T> void _print(vector <T> v);
@@ -78,24 +79,25 @@ template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
-#define INF 1000000000000000000
-const int mod = 1000000007;
-const int mod1 = 998244353;
-const int N = 200005;
+// --- CONSTANTS ---
+constexpr ll INF = 1e18;       // Use for long long
+constexpr int INF32 = 1e9 + 7; // Use for 32-bit int
+constexpr int mod = 1000000007;
+constexpr int mod1 = 998244353;
+constexpr int N = 100007;
 
 void solve(){
-	
-}
+    
+}  
 
-signed main(void){
+int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     freopen("error.txt", "w", stderr);
 	#endif
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+    
+    cin.tie(0)->sync_with_stdio(0);
     int T; cin >> T;
     while(T--){
         solve();
